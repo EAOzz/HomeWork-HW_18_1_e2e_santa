4cb3c1f29913f53f76d0e071cef188274f2d3f06
EAOzz <katrinkaya86@gmail.com>
2023-02-02 23:06:42 +0300

    Added 18.1 finish

--- cypress/e2e/santa-box-create.cy.js
+++ cypress/e2e/santa-box-create.cy.js
@@ -109,17 +109,13 @@ describe("user can create a box and run it", () => {
    after("delete box", () => {
     cy.visit("/login");
     cy.login(users.userAutor.email, users.userAutor.password);
-    cy.get(
-      '.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account/boxes"] > .header-item > .header-item__text > .txt--med'
-    ).click();
-    cy.get(":nth-child(1) > a.base--clickable > .user-card").first().click();
-    cy.get(
-      ".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner"
-    ).click();
+    cy.get(dashboardPage.boxButton).last().click();
+    cy.get(boxPage.boxCardButton).first().click();
+    cy.get(boxPage.boxMenuButton).last().click();
     cy.contains("Архивация и удаление").click({ force: true });
-    cy.get(":nth-child(2) > .form-page-group__main > .frm-wrapper > .frm").type(
+    cy.get(boxPage.boxDeleteField).type(
       "Удалить коробку"
     );
-    cy.get(".btn-service").click();
+    cy.get(boxPage.doxDeleteConfirmationButton).click();
   });
 });
