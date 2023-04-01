40a7d3b27cabe9a0119cfe035b3b4bd3885123e3
EAOzz <katrinkaya86@gmail.com>
2023-04-01 16:35:36 +0300

    Added allure

--- cypress.config.js
+++ cypress.config.js
@@ -2,23 +2,24 @@ const { defineConfig } = require("cypress");
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://staging.lpitko.ru",
     env:{
-      keyBox: '123',
-      inviteLink: ''
+      allureReuseAfterSpec: true
     },
     testIsolation: false,
     specPattern: "**/*.feature",
-    setupNodeEvents(on, config) {
+    setupNodeEvents (on, config) {
       const bundler = createBundler({
         plugins: [createEsbuildPlugin(config)],
         });
-        
         on("file:preprocessor", bundler);
         addCucumberPreprocessorPlugin(on, config);
+
+        allureWriter(on, config);
         return config;
     },
   },
